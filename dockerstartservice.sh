b7781fb8af194ce5e4b8c257043da44f47b67e11
zhs007 <sssxueren@gmail.com>
2019-07-31 16:47:47 +0800

    upd

--- dockerstartservice.sh
+++ dockerstartservice.sh
@@ -1,3 +1,3 @@
 docker stop adarenderserv
 docker rm adarenderserv
-docker run -d -v $PWD/cfg:/usr/src/app/cfg --name adarenderserv -p 7052:7052 adarenderserv
+docker run -d -v $PWD/cfg:/usr/src/app/cfg --name adarenderserv -p 7052:7052 adarender
