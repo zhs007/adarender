af4f9edfc1ac39736f6133e18e3171e713f50130
zerro <zerrozhao@gmail.com>
2020-02-01 17:31:49 +0800

    - Added telegraph.upload

---
+++ src/telegraph/upload.js
@@ -0,0 +1,93 @@
+const request = require('request');
+const fs = require('fs');
+
+/**
+ * parsefn - parse filename
+ * @param {string} fn - filename
+ * @return {object} ret - {uploadfn, type}
+ */
+function parsefn(fn) {
+  const nfn = fn.toLowerCase();
+  const arr = nfn.split('/');
+  const ufn = arr[arr.length - 1];
+  const arr1 = ufn.split('.');
+  if (arr1[arr1.length - 1] == 'jpg' || arr1[arr1.length - 1] == 'jpeg') {
+    return {uploadfn: ufn, type: 'image/jpeg'};
+  } else if (arr1[arr1.length - 1] == 'png') {
+    return {uploadfn: ufn, type: 'image/png'};
+  }
+
+  return undefined;
+}
+
+/**
+ * upload - upload a file
+ * @param {string} fn - filename
+ * @param {number} timeout - timeout in second
+ * @return {Promise<object>} ret - {error, buf}
+ */
+function upload(fn, timeout) {
+  return new Promise((resolve, reject) => {
+    const fnret = parsefn(fn);
+    if (!fnret) {
+      resolve({error: new Error('telegraph.upload invalid file type.')});
+
+      return;
+    }
+
+    request
+        .post(
+            {
+              url: 'https://telegra.ph/upload',
+              formData: {
+                custom_file: {
+                  value: fs.createReadStream(fn),
+                  options: {
+                    filename: fnret.uploadfn,
+                    contentType: fnret.type,
+                  },
+                },
+              },
+            },
+            (err, response, body) => {
+              if (err) {
+                resolve({error: err});
+
+                return;
+              }
+
+              try {
+                const ret = JSON.parse(body);
+                if (!ret) {
+                  resolve({
+                    error: new Error('telegraph.upload invalid body. ' + body),
+                  });
+
+                  return;
+                }
+
+                if (!Array.isArray(ret)) {
+                  resolve({
+                    error: new Error(
+                        'telegraph.upload invalid body (not array). ' + body,
+                    ),
+                  });
+
+                  return;
+                }
+
+                resolve({url: ret[0].src});
+              } catch (err) {
+                resolve({error: err});
+              }
+            },
+        )
+        .on('error', (err1) => {
+          resolve({error: err1});
+
+          return;
+        });
+  });
+}
+
+exports.upload = upload;
